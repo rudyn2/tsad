c9e1d0be81f6182116b63d05cae101eb0acb0a67
Johnny952 <jonathan.espinola@ug.uchile.cl>
2021-04-22 08:01:22 -0400

    changed evaluation and trainning evaluation

--- scripts/train.py
+++ scripts/train.py
@@ -8,14 +8,17 @@ from torch.utils.data import DataLoader, random_split
 
 def train_for_classification(net, dataset, optimizer,
                             seg_criterion, tl_criterion, va_criterion,
-                            criterion_weights=[1, 1, 1],
+                            criterion_weights=[1/3, 1/3, 1/3],
                             lr_scheduler=None,
                             epochs: int = 1,
                             batch_size: int = 64,
                             reports_every: int = 1,
                             device: str = 'cuda',
                             val_percent: float = 0.1,
-                            use_wandb= False):
+                            use_wandb = False,
+                            va_weights = None):
+    if va_weights is None:
+        va_weights = torch.Tensor([0.5, 0.5]).to(device)
     net.to(device)
     n_val = int(len(dataset) * val_percent)
     n_train = len(dataset) - n_val
@@ -34,8 +37,8 @@ def train_for_classification(net, dataset, optimizer,
         net.train()
 
         # Variables para las métricas
-        running_loss, running_acc = 0.0, 0.0
-        avg_acc, avg_loss = 0, 0
+        running_loss, running_tl_acc, running_seg_acc, running_va_loss = 0.0, 0.0, 0.0, 0.0
+        avg_tl_acc, avg_seg_acc, avg_va_loss, avg_loss = 0, 0, 0, 0
 
         for i, (x, s, tl, v_aff) in enumerate(train_loader):
 
@@ -58,35 +61,46 @@ def train_for_classification(net, dataset, optimizer,
 
             # accuracy of traffic lights
             _, max_idx = torch.max(y['traffic_light_status'], dim=1)
-            running_acc += torch.sum(max_idx == tl).item()
-            avg_acc = running_acc / items * 100
+            running_tl_acc += torch.sum(max_idx == tl).item()
+            avg_tl_acc = running_tl_acc / items * 100
+            # accuracy semantic
+            _, max_idx = torch.max(y['segmentation'], dim=1)
+            running_seg_acc += torch.sum(max_idx == s).item() / max_idx.numel()
+            avg_seg_acc = running_seg_acc / items * 100
+            # error of vehicle affordances
+            running_va_loss += torch.sum(((y['vehicle_affordances'] - v_aff).squeeze()*va_weights)**2).item()
+            avg_va_loss = running_va_loss / items
 
             # report
             sys.stdout.write(f'\rEpoch:{e}({items}/{n_train}), '
                              + (f'lr:{lr_scheduler.get_last_lr()[0]:02.7f}, ' if lr_scheduler is not None else '')
                              + f'Loss:{avg_loss:02.5f}, '
-                             + f'Train TL Acc:{avg_acc:02.1f}%')
+                             + f'Train SEG Acc:{avg_seg_acc:02.1f}%, '
+                             + f'Train TL Acc:{avg_tl_acc:02.1f}%, '
+                             + f'Train VA Loss: {avg_va_loss:02.5f}')
             if use_wandb:
-                wandb.log({'train/loss': float(avg_loss), 'train/acc TL': float(avg_acc)}, step=global_step)
+                wandb.log({'train/loss': float(avg_loss), 'train/acc TL': float(avg_tl_acc), 'train/acc SEG': float(avg_seg_acc), 'train/loss VA': float(avg_va_loss)}, step=global_step)
             global_step += 1
 
         tiempo_epochs += time.time() - inicio_epoch
         if use_wandb:
-            wandb.log({'train/loss': float(avg_loss), 'train/acc TL': float(avg_acc), 'epoch': e})
+            wandb.log({'train/loss': float(avg_loss), 'train/acc TL': float(avg_tl_acc), 'train/acc SEG': float(avg_seg_acc), 'train/loss VA': float(avg_va_loss), 'epoch': e})
 
         if e % reports_every == 0:
             sys.stdout.write(', Validating...')
 
             train_loss.append(avg_loss)
-            train_acc.append(avg_acc)
+            train_acc.append([avg_tl_acc, avg_seg_acc, avg_va_loss])
 
-            avg_loss = eval_net(device, net, val_loader, seg_criterion, tl_criterion, va_criterion, criterion_weights=criterion_weights)
-            test_loss.append(avg_loss)
-            sys.stdout.write(f', Val Acc:{avg_loss:02.2f}%, '
+            avg_tl_acc, avg_seg_acc, avg_va_loss = eval_net(device, net, val_loader, va_weights)
+            test_loss.append([avg_tl_acc, avg_seg_acc, avg_va_loss])
+            sys.stdout.write(f', Val TL Acc:{avg_tl_acc:02.2f}%, '
+                             + f'Val SEG Acc:{avg_seg_acc:02.2f}%, '
+                             + f'Val VA Loss:{avg_va_loss:02.5f}%, '
                              + f'Avg-Time:{tiempo_epochs / e:.3f}s.\n')
             if use_wandb:
-                wandb.log({'val/acc': float(avg_loss)}, step=global_step)
-                wandb.log({'val/acc': float(avg_loss), 'epoch': e})
+                wandb.log({'val/acc TL': float(avg_tl_acc), 'val/acc SEG': float(avg_seg_acc), 'val/loss VA': float(avg_va_loss)}, step=global_step)
+                wandb.log({'val/acc TL': float(avg_tl_acc), 'val/acc SEG': float(avg_seg_acc), 'val/loss VA': float(avg_va_loss), 'epoch': e})
 
             # checkpointing
             if avg_loss <= best_loss:
@@ -105,25 +119,36 @@ def train_for_classification(net, dataset, optimizer,
     return train_loss, (train_acc, test_loss)
 
 
-def eval_net(device, net, test_loader,
-                seg_criterion, tl_criterion, va_criterion,
-                criterion_weights=[1, 1, 1]):
+def eval_net(device, net, test_loader, va_weights):
     net.eval()
-    running_loss = 0.0
+    running_tl_acc, running_seg_acc, running_va_loss = 0.0, 0.0, 0.0
     total_test = 0
 
     for i, (x, s, tl, v_aff) in enumerate(test_loader):
         x, s, tl, v_aff = x.to(device), s.to(device), tl.to(device), v_aff.to(device)
 
+        with torch.no_grad():
+            y = net(x)
+
         l1 = seg_loss(y['segmentation'], s)
         l2 = tl_loss(y['traffic_light_status'], tl)
         l3 = va_loss(y['vehicle_affordances'], v_aff)
 
-        running_loss += criterion_weights[0]*l1 + criterion_weights[1]*l2 + criterion_weights[2]*l3
-        total_test += len(labels)
+        # accuracy of traffic lights
+        _, max_idx = torch.max(y['traffic_light_status'], dim=1)
+        running_tl_acc += torch.sum(max_idx == tl).item()
+        # accuracy semantic
+        _, max_idx = torch.max(y['segmentation'], dim=1)
+        running_seg_acc += torch.sum(max_idx == s).item() / max_idx.numel()
+        # error of vehicle affordances
+        running_va_loss += torch.sum(((y['vehicle_affordances'] - v_aff).squeeze()*va_weights)**2).item()
 
-    avg_loss = (running_loss / total_test) * 100
-    return avg_loss
+        total_test += x.shape[0]
+
+    avg_tl_acc = (running_tl_acc / total_test) * 100
+    avg_seg_acc = (running_seg_acc / total_test) * 100
+    avg_va_loss = running_va_loss / total_test
+    return avg_tl_acc, avg_seg_acc, avg_va_loss
 
 
 
@@ -154,7 +179,7 @@ if __name__ == "__main__":
     seg_loss = FocalLoss(apply_nonlin=torch.sigmoid)
     tl_loss_weights = torch.tensor([0.2, 0.8]).to(device)
     tl_loss = nn.BCEWithLogitsLoss(pos_weight=tl_loss_weights)
-    va_loss = nn.MSELoss()
+    va_loss = nn.MSELoss()                  # esta explotando
 
     optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
@@ -166,4 +191,5 @@ if __name__ == "__main__":
                             batch_size = 1,
                             reports_every = 1,
                             device = device,
-                            val_percent = 0.1)
+                            val_percent = 0.1,
+                            va_weights = tl_loss_weights)
