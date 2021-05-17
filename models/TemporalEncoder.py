5f8f8bb20fc079b9fd96e99b64a9c4917e86ecdb
Johnny952 <jonathan.espinola@ug.uchile.cl>
2021-05-17 12:16:48 -0400

    ConvLSTM output mean

--- models/TemporalEncoder.py
+++ models/TemporalEncoder.py
@@ -41,6 +41,7 @@ class RNNEncoder(nn.Module):
         h = None
         y, h = self.lstm(x_pack, h)
         # Output of lstm is stacked through all outputs (#outputs == #inputs), we get last output
-        y = y.data.view(embedding.shape)[:, -1, :, :, :].squeeze(dim=1)
-        
+        #y = y.data.view(embedding.shape)[:, -1, :, :, :].squeeze(dim=1)
+        y = torch.mean(y.data.view(embedding.shape), dim=1)
+
         return y
