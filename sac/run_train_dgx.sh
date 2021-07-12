e3e59f3d0244b7fc6b61ec38a2d0c89011f07469
rudyn2 <c01010010@hotmail.com>
2021-07-12 14:35:53 -0400

    minor changes

--- sac/run_train_dgx.sh
+++ sac/run_train_dgx.sh
@@ -2,4 +2,4 @@ python train.py --host 172.18.0.1 --num-seed 4000 --port 2004 --vehicles 100 --w
   --vis-weights '../dataset/weights/best_model_11_validation_accuracy=-1.6713.pt' \
    --temp-weights '../dataset/weights/best_VanillaRNNEncoder(2).pth' \
   --batch-size 512 --max-episode-steps 400 --eval-frequency 12 \
-  --num-train-steps 1e6 --num-eval-episodes 3 --online-memory-size 16384
+  --num-train-steps 1000000 --num-eval-episodes 3 --online-memory-size 16384
