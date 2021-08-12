05978d12566c2ef787668e62bd450efa59812e6c
rudyn2 <c01010010@hotmail.com>
2021-08-12 13:44:51 -0400

    fix speed logging, enable learnable temperature, updates gym-carla

--- sac/trainer.py
+++ sac/trainer.py
@@ -138,7 +138,7 @@ class SACTrainer(object):
             action = action_proxy(action)
             wandb.log({f"instant/action/{name}": value for name, value in zip(["throttle", "brake", "steer"],
                                                                               action)})
-            wandb.log({"instant/speed": np.mean(obs["speed"])})
+            wandb.log({"instant/speed": np.linalg.norm(obs["speed"])})
 
             # run training update
             if self.step >= self.num_seed_steps:
