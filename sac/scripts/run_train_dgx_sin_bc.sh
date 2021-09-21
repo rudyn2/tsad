100b6947c34b43ce32a92a8c615c9b19fe3d1a1c
Rudy <rudy.garcia@ug.uchile.cl>
2021-09-21 12:01:16 -0300

    update scripts

---
+++ sac/scripts/run_train_dgx_sin_bc.sh
@@ -0,0 +1,6 @@
+python ../train.py --host 172.18.0.1 \
+  --port 2004 \
+  --num-seed 4000  --vehicles 100 --walkers 100 \
+  --batch-size 256 --max-episode-steps 300 --eval-frequency 50 \
+  --reward-scale 10 --learn-temperature --act-mode pid \
+  --num-train-steps 1000000 --num-eval-episodes 3 --online-memory-size 25000
